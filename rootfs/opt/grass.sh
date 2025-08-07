a5f5324321ff4bb61bb8f738814f88074a50b51f
Alone <hi@anlo.ng>
2025-08-07 13:46:01 +0800

    add mihomo

--- rootfs/opt/grass.sh
+++ rootfs/opt/grass.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-if [ -z "$GRASS_PROXY" ]; then
+if [ -n "$GRASS_PROXY" ]; then
   export http_proxy=http://$PROXY_ADDR
   export https_proxy=http://$PROXY_ADDR
   export all_proxy=socks5://$PROXY_ADDR
