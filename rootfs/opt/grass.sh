bde0b494598a925e97e9de705bf1e97f561baa05
Alone <hi@anlo.ng>
2025-08-08 08:35:31 +0800

    add minimize

--- rootfs/opt/grass.sh
+++ rootfs/opt/grass.sh
@@ -6,7 +6,8 @@ if [ -n "$GRASS_PROXY" ]; then
   export http_proxy=http://$GRASS_PROXY
   export https_proxy=http://$GRASS_PROXY
   export all_proxy=socks5://$GRASS_PROXY
-  curl -v ipinfo.io
+  curl -v -o - ip.sb
+  curl -v -o - ipinfo.io
 fi
 
 grass
