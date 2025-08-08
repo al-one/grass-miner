3cf8b771055a7314fbf4c1837bbcf8674f52a4fb
Alone <hi@anlo.ng>
2025-08-08 08:46:47 +0800

    add mihomo

--- rootfs/opt/grass.sh
+++ rootfs/opt/grass.sh
@@ -6,8 +6,8 @@ if [ -n "$GRASS_PROXY" ]; then
   export http_proxy=http://$GRASS_PROXY
   export https_proxy=http://$GRASS_PROXY
   export all_proxy=socks5://$GRASS_PROXY
-  curl -v -o - ip.sb
-  curl -v -o - ipinfo.io
+  curl -v -o /proc/self/fd/2 ip.sb
+  curl -v -o /proc/self/fd/2 ipinfo.io
 fi
 
 grass
